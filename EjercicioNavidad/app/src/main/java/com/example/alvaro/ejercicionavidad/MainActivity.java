bff653c9e1aa9aba6d15fdc285cd36e2983bbc1e
Alvaro <roviraibanezalvaro@gmail.com>
2016-02-18 12:59:05 +0100

    Mejoras menu contextual

--- EjercicioNavidad/app/src/main/java/com/example/alvaro/ejercicionavidad/MainActivity.java
+++ EjercicioNavidad/app/src/main/java/com/example/alvaro/ejercicionavidad/MainActivity.java
@@ -202,6 +202,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent intentMain = new Intent(MainActivity.this ,acerca_de.class);
                 startActivity(intentMain);
                 return true;
+            case R.id.action_dibujo:
+                //Toast.makeText(MainActivity.this, "dibujo", Toast.LENGTH_SHORT).show();
+                Intent intentMain2 = new Intent(MainActivity.this,dibujo.class);
+                startActivity(intentMain2);
+                return true;
+            case R.id.action_listarPedido:
+                Intent intentMain3 = new Intent(MainActivity.this,listarPedidos.class);
+                startActivity(intentMain3);
+                return true;
             default:
                 return super.onContextItemSelected(item);
         }
